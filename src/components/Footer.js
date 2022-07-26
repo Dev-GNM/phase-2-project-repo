85ccc24d01d515e215a9eb6c1622ebfecf4c427a
Geoffrey <geoffreynjihia15@gmail.com>
2022-07-26 19:21:49 +0300

    Adding footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-export default function Footer() {
+ function Footer() {
   return (
-    <div>
-      
+    <div id="footer">
+      <div className="about-us">
+      <em><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut<br></br> 
+      labore et dolore magna aliqua. Ut enim ad minim veniam, <br></br>
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+      </p></em>
+      </div>
+      <div className="support">
+      <h3>We love to support you! Contact us</h3>
+      <form id="contact-us">
+
+      </form>
+      </div> 
     </div>
   );
 }
+
+
+export default Footer;
