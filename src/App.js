af2ad616c086dbbda231306a91f6bee3b1f8f4d0
Geoffrey <geoffreynjihia15@gmail.com>
2022-07-26 18:45:19 +0300

    Adding form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './index.css';
 import Slider from "./components/Slider"
 import Navbar from "./components/Navbar"
+import NewContent from "./components/NewContent"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Slider />
+      <NewContent />
     </div>
   );
 }
--- src/components/NewContent.js
+++ src/components/NewContent.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-export default function NewContent() {
+function NewContent() {
   return (
-    <div>
-      
+    <div id="content-contaner">
+      <div id="form-container">
+        <form id="content">
+        <label for="topictype">Topic:</label>
+    <input type="text" id="top" name="majortopic" placeholder="Your topic.."/>
+
+    <label for="topictype">Add content here:</label>
+    <textarea id="your-content" name="content" placeholder="Add content here"/>
+
+
+    <input type="submit" value="Submit"/>
+        </form>
+      </div>
+
+      <div id="render-content">
+        <h2>Empty div</h2>
+      </div>
     </div>
   );
 }
+
+export default NewContent;
