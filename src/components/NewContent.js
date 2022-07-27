3c3b7c16aacfc5f427dc9b7e2c798c00964e1a7c
Geoffrey <geoffreynjihia15@gmail.com>
2022-07-27 13:20:32 +0300

    Fetching content

--- src/components/NewContent.js
+++ src/components/NewContent.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
-function NewContent() {
+
+function NewContent({ content, onDeleteContent }) {
+  const { id, topic, content, author } = content;
+
+  const [ isRead, setisRead ] = useState(false);
+
+  const toggleRead = () => {
+    setisRead((isRead) => !isRead);
+  };
+  function onClickDelete() {
+    fetch(`http://localhost:8004/poems/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => {
+        onDeleteContent(id);
+      });
+  }
+
   return (
     <div id="content-container">
       <div id="form-container">
