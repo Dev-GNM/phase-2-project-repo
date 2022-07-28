4f5d92f01c62a2d7bef84152fa2857730366b6b0
Geoffrey <geoffreynjihia15@gmail.com>
2022-07-28 12:47:28 +0300

    styling contact us form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,29 +12,14 @@ import React from 'react';
       <div className="support">
       <h3>We love to here from you! Contact us</h3>
       <form id="contact-us">
-      <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
-
-    <label for="lname">Last Name</label>
-    <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
-
-    <label for="country">Country</label>
-    <select id="country" name="country">
-      <option value="australia">Kenya</option>
-      <option value="canada">Uganda</option>
-      <option value="usa">Tanzania</option>
-    </select>
-
-    <label for="subject">Subject</label>
-    <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
-
-    <input type="submit" value="Submit"/>
-
+    
+   
+    
       </form>
       </div> 
     </div>
-  );
-}
+ )};
+
 
 
 export default Footer;
--- src/components/NewContent.js
+++ src/components/NewContent.js
@@ -1,53 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-
-function NewContent({ content, onDeleteContent }) {
-  const { id, topic, content, author } = content;
-
-  const [ isRead, setisRead ] = useState(false);
-
-  const toggleRead = () => {
-    setisRead((isRead) => !isRead);
-  };
-  function onClickDelete() {
-    fetch(`http://localhost:8004/poems/${id}`, {
-      method: "DELETE",
+function NewContent({ onAddContent }) {
+  const [formData, setFormData] = useState({
+    topic: "",
+    content: "",
+    author: "",
+  });
+  function handlechange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("http://localhost:8004/poems", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: formData.title,
+        content: formData.content,
+        author: formData.author,
+      }),
     })
       .then((r) => r.json())
-      .then(() => {
-        onDeleteContent(id);
+      .then((newContent) => {
+        onAddContent(newContent);
+        setFormData({ ...formData, title: "", author: "", content: "" });
       });
   }
-
   return (
-    <div id="content-container">
-      <div id="form-container">
-        <form id="content">
-        <label for="topictype">Topic:</label>
-    <input type="text" id="top" name="majortopic" placeholder="Your topic.."/>
-
-    <label for="topictype">Add content here:</label>
-    <textarea id="your-content" name="content" placeholder="Add content here..."/>
-
-
-    <input type="submit" value="Submit"/>
-        </form>
-      </div>
-
-      <div id="render-content">
-      <h3>{topic}</h3>
-      <p>{content}</p>
-      <p>
-        <strong>- {author}</strong>
-      </p>
-      <button onClick={toggleRead}>
-        {isRead ? "Read" : "Mark as Read and share"}
-      </button>
-      <button onClick={onClickDelete} style={{ marginLeft: "10px" }}>
-        Delete
-      </button>
-    </div>
-    </div>
+    <form onSubmit={handleSubmit} className="new-content-form">
+      <input
+        value={formData.title}
+        name="topic"
+        onChange={handlechange}
+        placeholder="Topic"
+      />
+      <input
+        value={formData.author}
+        name="author"
+        onChange={handlechange}
+        placeholder="Author"
+      />
+      <textarea
+        value={formData.content}
+        name="content"
+        onChange={handlechange}
+        placeholder="Write your content here..."
+        rows={10}
+      />
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
